4e69ccc795dd880600d24045fd902125247b4fbd
sokleng <sokleng.phen@innotechsolution.tech>
2026-07-05 16:13:37 +0700

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-@//ts-expect-error
 import "./globals.css";
 import { ThemeProvider } from "@/components/darkmode/theme-provider";
 
